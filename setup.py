058c5e6e4d729cbbc4f0383b89d97c8acaf6735c
MaxNollet <mjh.nollet@student.han.nl>
2021-04-22 19:27:26 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="gaps",
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "Flask"
+    ]
+)
