9db3dbb23e9bb1092ebe7c03e2709deb2f935b7a
Lean Schoonveld <lean.schoonveld@gmail.com>
2021-05-13 17:20:36 +0200

    Features: - Leest het bestand in - Bij specifieke kolommen worden andere acties gedaan - File not found error exception
    TODO
    - Parse het bestand
    - Hernoemen van het bestand
    
    BUGFIXES
    - Bug gefixt waardoor inheritance en afkorting niet samen waren gekoppeld

--- Parser/parser.py
+++ Parser/parser.py
@@ -43,33 +43,75 @@ def reader(file, headers):
                         aliases.append(al)
                 if "GenePanel" == value[0]:
                     panels = []  # list with 'LENGTE','MR', 'SCHISIS' eg
+                    # currently not in use
                     ihh_list = []  # list with inheritance
+                    combi_panel = []  # combi [OMIM, AR, AD] example
                     # print(line.strip().split("\t")[key_index]) # eg
-                    p = re.findall(f"(?<=\().+?(?=\))",
-                                   line.strip().split("\t")[key_index])
-                    # the inheritance between ()
-                    if len(p) >= 1:
-                        for ih in p:  # p = ['AD', 'AD',
-                            # 'AD;UK,AR,AD,XL', 'AD']
-                            if ";" or "," in str(ih):  # ih 'AD;UK,XL'
-                                ihh = str(ih).replace(";", ",").split(",")
-                                for j in ihh:
-                                    ihh_list.append(InheritanceType(type=j))
-                            else:
-                                for j in ih:  # example 'AD'
-                                    ihh_list.append(InheritanceType(type=j))
+                    haken = re.findall(f"(?<=\().+?(?=\))",
+                                       line.strip().split("\t")[key_index])
+                    print(haken, "HAKEN")
+                    if re.search(";", str(haken)):
+                        for h in haken:
+                            print(h, " h")
+                            testje = h.replace(";", ",")
+                            # print(line.strip().split("\t")[key_index])  # eg
+                            test = re.sub(h, testje,
+                                          line.strip().split("\t")[key_index])
+                            test = test.split(";")
+                            print(test, "testen")
+                            # print(test)
+                    else:
+                        # print(line.strip().split("\t")[key_index])  # eg
+                        test = line.strip().split("\t")[key_index].split(";")
+                    for te in test:
+                        een_genpanel = []
+                        p = re.findall(f"(?<=\().+?(?=\))",
+                                       te)
+                        print(test)
+                        print(p, "pp")
+                        # print(p)
+                        # the inheritance between ()
+                        # print(p)
+                        # test = line.strip().split("\t")[key_index].split(";")
+                        # print(test)
+                        print(te, "te")
+                        if len(p) >= 1:
+                            # print(p, "P")
+                            for ih in p:  # p = ['AD', 'AD',
+                                # 'AD;UK,AR,AD,XL', 'AD']
+                                # print(ih, " IH")
+                                if ";" or "," in str(ih):  # ih 'AD;UK,XL'
+                                    ihh = str(ih).replace(";", ",").split(",")
+                                    for j in ihh:
+                                        print(j, "jjjjj")
+                                        een_genpanel.append(
+                                            InheritanceType(type=j))
+                                        # ihh_list.append(InheritanceType(type=j))
+                                else:
+                                    for j in ih:  # example 'AD'
+                                        een_genpanel.append(
+                                            InheritanceType(type=j))
+                                        # ihh_list.append(InheritanceType(type=j))
 
-                    t = re.sub(f"(?<=\().+?(?=\))", "",
-                               str(line.strip().split("\t")[key_index]))
-                    k = t.replace(" ()", "").replace("\"", "").split(";")
-                    for a in k:  # k contains ['HEMOS', 'OMIM'] example
-                        gp = Genepanel(abbreviation=a)
-                        panels.append(gp)
-                    # print(re.findall(f".+?(?=\s\()", t))
+                        t = re.sub(f"(?<=\().+?(?=\))", "", te)
+                        # t = re.sub(f"(?<=\().+?(?=\))", "", str(line.strip().split("\t")[key_index]))
+                        k = t.replace(" ()", "").replace("\"", "").split(";")
+                        # k = t.replace(" ()", "").replace("\"", "").split(";")
+                        for a in k:  # k contains ['HEMOS', 'OMIM'] example
+                            gp = Genepanel(abbreviation=a)
+                            panels.append(gp)
+                            een_genpanel.append(gp)
+                        combi_panel.append(een_genpanel)
+                        # print(re.findall(f".+?(?=\s\()", t))
+            # fi = FileInfo(gene=gene, alias=aliases, p_symbol=p_symbol,
+            #               panel=panels, p_inheritance=ihh_list)
             fi = FileInfo(gene=gene, alias=aliases, p_symbol=p_symbol,
-                          panel=panels, p_inheritance=ihh_list)
+                          panel=combi_panel)
             file_list.append(fi)  # kan niet in 1 regel
     print("complete")
+    # print(file_list)
+    print(file_list[len(file_list) - 6])
+    print(file_list[len(file_list) - 1])
     return tuple(file_list)
 
 
@@ -77,8 +119,8 @@ def reader(file, headers):
 class FileInfo:
     gene: Gene
     p_symbol: GenepanelSymbol
-    panel: list = field(default_factory=list)
-    p_inheritance: list = field(default_factory=list)
+    panel: list = field(default_factory=list)  # includes AR, AD en afkorting
+    # p_inheritance: list = field(default_factory=list)
     alias: list = field(default_factory=list)
 
 
