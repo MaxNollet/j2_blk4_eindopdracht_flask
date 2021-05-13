9d78368726fd6ca64129ff7ba46abab317a2af7f
Lean Schoonveld <lean.schoonveld@gmail.com>
2021-05-13 15:29:05 +0200

    Features: - Leest het bestand in - Bij specifieke kolommen worden andere acties gedaan - File not found error exception
    TODO
    - Parse het bestand
    - Hernoemen van het bestand

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,126 +1,97 @@
-from dataclasses import dataclass
-from gaps.models import Gene, Alias, FileInfo, Genepanel, GenepanelSymbol
+from dataclasses import dataclass, field
+from gaps.models import Gene, Alias, Genepanel, GenepanelSymbol, \
+    InheritanceType
 import re
 
 
 def reader(file, headers):
     """
     Reads the GenePanel file, gets the headers from the first row
-    :param file: GenPanelOverzicht | tsv file | first row needs to contain
+    :param file: GenPanelOverzicht | csv file | first row needs to contain
     the names of the columns
     :param headers: names of the columns to get the data from
     :return:
     """
-    data = {}  # is this the correct data structure?
-    # Gene(id="")
-    # Gene object contains: GeneID
-    data_test = []
-    t_data = {}
-    t = []  # test
-    tt = []  # object test
+    print("reading file")
+    data = {}  # index and name column
+    file_list = []  # Contains objects from FileInfo
     with open(file, mode="r", encoding="utf-8-sig") as f:
-        # https://stackoverflow.com/questions/17912307/u-ufeff-in-python-string
-        # data = [i for i, j in enumerate(f.readline().strip().split(";")) if "GenePanels" in j]
-        # how to replace "GenePanels" with h? without setting the entire line in the loop
-        # for i, j in enumerate(f.readline().strip().split(";")):
+        # https://stackoverflow.com/questions/17912307/
+        # u-ufeff-in-python-string
+        # data = [i for i, j in enumerate(f.readline().strip()
+        # .split(";")) if "GenePanels" in j]
         for i, j in enumerate(f.readline().strip().split("\t")):
             # i is the index and j is the value
             for h in headers:
-                # print(h, j)
-                if h in j:
+                if h == j:  # headers needs to match with column
                     # data.update({i: j})  # hier staat er \ufeff voor
                     data[i] = [j]
         for line in f:
             gene = Gene(in_genepanel=True)
             p_symbol = GenepanelSymbol()
-            id = False
-
             for key_index, value in data.items():  # beter naam voor
-
-                # print(headers[key_index])
-                # print(value)
-                # value nodig beetje karig het is nu gwn een list waar alle data van een kolom in komt
-                # print(key, value)
-                # value.append(line.strip().split("\t")[key_index])
-                # if re.findall("(?<=_).+?(?=\])", headers[key_index]) == "ncbi":
-                # if re.search("NCBI", headers[key_index]):
-                #     gene.ncbi_gene_id = line.strip().split("\t")[key_index]
-                #     id = True
-                # if re.search("NCBI", value[0]):
-                #     print(key_index, value)
                 if re.search("HGNC", value[0]):
                     gene.hgnc_symbol = line.strip().split("\t")[key_index]
-                    id = True
                 if re.search("GenePanels_Symbol", value[0]):
                     p_symbol.symbol = line.strip().split("\t")[key_index]
-                if re.search("Aliases", value[0]) and id == True:
+                if re.search("Aliases", value[0]):
                     aliases = []
                     # print(line.strip().split("\t")[key_index].split("|"))
                     for alias in line.strip().split("\t")[key_index].split(
                             "|"):
                         al = Alias(hgnc_symbol=alias)
                         aliases.append(al)
-                    # print(line.strip().split("\t")[key_index])
-                # t.append(aliases)
-                value.append(gene)
-            fi = FileInfo(gene=gene, alias=aliases, p_symbol=p_symbol)
-            tt.append(fi)  # kan niet in 1 regel
-            data_test.append(gene)
+                if "GenePanel" == value[0]:
+                    panels = []  # list with 'LENGTE','MR', 'SCHISIS' eg
+                    ihh_list = []  # list with inheritance
+                    # print(line.strip().split("\t")[key_index]) # eg
+                    p = re.findall(f"(?<=\().+?(?=\))",
+                                   line.strip().split("\t")[key_index])
+                    # the inheritance between ()
+                    if len(p) >= 1:
+                        for ih in p:  # p = ['AD', 'AD',
+                            # 'AD;UK,AR,AD,XL', 'AD']
+                            if ";" or "," in str(ih):  # ih 'AD;UK,XL'
+                                ihh = str(ih).replace(";", ",").split(",")
+                                for j in ihh:
+                                    ihh_list.append(InheritanceType(type=j))
+                            else:
+                                for j in ih:  # example 'AD'
+                                    ihh_list.append(InheritanceType(type=j))
 
-            # f = FileInfo(gene=gene)
-
-            # t.append(FileInfo(gene=gene))
-
-    print(len(data.get(1)))
-    print(len(data_test))
-    # print(data_test)
-    # print(t)
-    print(len(tt))
-    print(tt)
-    # print(data.get(0)[1])
-    # t = [Gene(ncbi_gene_id=data.get(0)[1])]
-    # a = [Gene(ncbi_gene_id=data.get(0)[1])]
-    # a[0].hgnc_symbol=data.get(1)[1]
-    # print(a)
-    # print(a[0])
-
-    # tt = ParseItems(ncbi_gene_id=int(data.get(0)[1]))
-    # print(tt)
-
-    # print(len(data.get(0)))
-    # print(data)
-    for keyword, value in data.items():
-        print(keyword)
-
-    print("Voltooid")
-
-
-@dataclass()
-class ParseItems:
-    ncbi_gene_id: int
-    hgnc_symbol = str
-    alias = list
-    in_panel = bool
+                    t = re.sub(f"(?<=\().+?(?=\))", "",
+                               str(line.strip().split("\t")[key_index]))
+                    k = t.replace(" ()", "").replace("\"", "").split(";")
+                    for a in k:  # k contains ['HEMOS', 'OMIM'] example
+                        gp = Genepanel(abbreviation=a)
+                        panels.append(gp)
+                    # print(re.findall(f".+?(?=\s\()", t))
+            fi = FileInfo(gene=gene, alias=aliases, p_symbol=p_symbol,
+                          panel=panels, p_inheritance=ihh_list)
+            file_list.append(fi)  # kan niet in 1 regel
+    print("complete")
+    return tuple(file_list)
 
 
 @dataclass
 class FileInfo:
     gene: Gene
-    # panel: Genepanel
-    # alias: List[Alias]
     p_symbol: GenepanelSymbol
+    panel: list = field(default_factory=list)
+    p_inheritance: list = field(default_factory=list)
     alias: list = field(default_factory=list)
 
 
 def main():
     # headers = ["GeneID_NCBI", "Symbol_HGNC", "Aliases"]
-    headers = ["GeneID_NCBI", "Symbol_HGNC", "Aliases", "GenePanels_Symbol"]
-    # headers = ["GeneID_NCBI", "Symbol_HGNC", "Aliases", "GenePanels_Symbol"]
-    file = "/Users/lean/Documenten/School/Flask/Course8_project/Parser/GenPanelOverzicht_edited.csv"
-    file = "/Users/lean/Documenten/School/Flask/Course8_project/Parser/GenPanelOverzicht_DG-3.1.0_HAN_original_tsv.txt"
-    reader(file, headers)
-    # f = open(file)
-    # print(f.read())
+    headers = ["GeneID_NCBI", "Symbol_HGNC", "Aliases", "GenePanels_Symbol",
+               "GenePanel"]
+    file = "/Users/lean/Documenten/School/Flask/Course8_project/" \
+           "Parser/GenPanelOverzicht_DG-3.1.0_HAN_original_tsv.txt"
+    try:
+        reader(file, headers)
+    except FileNotFoundError:
+        print("File not found! ->", file)
 
 
 main()
