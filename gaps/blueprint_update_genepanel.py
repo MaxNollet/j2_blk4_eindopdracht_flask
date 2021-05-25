03a5cde077109592e920d403fa47b0ad79259387
MaxNollet <mjh.nollet@student.han.nl>
2021-05-25 11:26:01 +0200

    Laatste commit voor de presentatie

--- gaps/blueprint_update_genepanel.py
+++ gaps/blueprint_update_genepanel.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template
 from gaps.models import db, Gene, Alias, Genepanel, t_gene_alias
 from sqlalchemy import func
 
+from gaps.genelogic.DatabaseInserter import update_genepanel_v2
+
 blueprint_update_genepanel = Blueprint("blueprint_update_genepanel", __name__)
 
 
@@ -11,9 +13,7 @@ def update_genepanel():
     """A function which handles requests for the '/update_genepanel'
        -route for the webapp.
     """
-    # count_genes = Gene.query.count()
-    # count_aliases = Alias.query.count()
-    # count_genepanels = Genepanel.query.count()
+    # update_genepanel_v2()
     # test = Alias.query.join(t_gene_alias).join(Alias).filter(t_gene_alias.c.alias_id == Alias.id).all()
     # print(test)
     # print(db.session.query(func.count(Gene.id)).group_by(Gene.genepanels).all())
@@ -21,4 +21,8 @@ def update_genepanel():
     # print(f"Genes: {count_genes}")
     # print(f"Aliases: {count_aliases}")
     # print(f"Genepanels: {count_genepanels}")
-    return render_template("template_update_genepanel.html", active_nav="genepanel")
+    return render_template("template_update_genepanel.html",
+                           active_nav="genepanel",
+                           unique_genes=Gene.query.count(),
+                           unique_aliases=Alias.query.count(),
+                           unique_genepanels=Genepanel.query.count())
