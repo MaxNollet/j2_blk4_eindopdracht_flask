a8c0ac5da7cc29d3255b016fe57ca39297792421
MaxNollet <mjh.nollet@student.han.nl>
2021-05-29 17:20:55 +0200

    (#3) redundante code verwijderd en statements toegevoegd om een heel genepanel in de db te zetten, kan nog geen relaties leggen tussen tabellen

--- gaps/genelogic/DatabaseInserter.py
+++ gaps/genelogic/DatabaseInserter.py
@@ -19,33 +19,51 @@ class DatabaseInserter:
     stmt_genepanel_symbol_insert = insert(GenepanelSymbol).on_conflict_do_nothing()
     stmt_genepanel_symbol_select = select(GenepanelSymbol.symbol, GenepanelSymbol.id).where(
         GenepanelSymbol.symbol.in_(bindparam("values")))
+    stmt_genepanel_insert = insert(Genepanel).on_conflict_do_nothing()
+    stmt_genepanel_select = select(Genepanel.abbreviation, Genepanel.id).where(
+        Genepanel.abbreviation.in_(bindparam("values")))
+    stmt_inheritance_type_insert = insert(InheritanceType).on_conflict_do_nothing()
+    stmt_inheritance_type_select = select(InheritanceType.type, InheritanceType.id).where(
+        InheritanceType.type.in_(bindparam("values")))
 
     def __init__(self):
         self.session: Session = db.session
 
-    def insert_genepanel(self, rows: List[Mapping[str, Mapping[str, str]]]):
-        genes: List[dict] = list()
-        aliases: List[dict] = list()
-        genepanel_symbols: List[dict] = list()
-        for row in rows:
-            gene = row.get("gene")
-            if gene is not None and len(gene) > 0:
-                genes.append(gene)
-            alias = row.get("aliases")
-            if alias is not None and len(alias) > 0:
-                aliases.extend(alias)
-            genepanel_symbol = row.get("genepanel_symbol")
-            if genepanel_symbol is not None:
-                genepanel_symbols.append(genepanel_symbol)
+    def insert_genepanel(self, file):
+        # New improved data structures for the db.
+        all_genes = list()
+        all_aliases = list()
+        all_genepanel_symbols = list()
+        all_genepanels = list()
+        all_inheritace_types = list()
+        # Extract variables and seperate into categories.
+        for line in file:
+            gene = line.gene
+            all_genes.append({"ncbi_gene_id": gene.ncbi_gene_id, "hgnc_symbol": gene.hgnc_symbol, "in_genepanel": True})
+            for alias in line.alias:
+                if alias.hgnc_symbol is not None and alias.hgnc_symbol != "":
+                    all_aliases.append({"hgnc_symbol": alias.hgnc_symbol})
+            all_genepanel_symbols.append({"symbol": line.p_symbol.symbol})
+            for panel in line.panel:
+                [all_inheritace_types.append({"type": element.type}) for element in panel[:-1]]
+                all_genepanels.append({"abbreviation": panel[-1].abbreviation})
+        # Generate combinations
+
         starttijd = time.perf_counter()
 
-        gene_ids = self.insert(insert_stmt=self.stmt_gene_insert, values=genes, return_ids=True,
+        gene_ids = self.insert(insert_stmt=self.stmt_gene_insert, values=all_genes, return_ids=True,
                                select_stmt=self.stmt_gene_select, col_as_key="hgnc_symbol")
-        alias_ids = self.insert(insert_stmt=self.stmt_alias_insert, values=aliases, return_ids=True,
+        alias_ids = self.insert(insert_stmt=self.stmt_alias_insert, values=all_aliases, return_ids=True,
                                 select_stmt=self.stmt_alias_select, col_as_key="hgnc_symbol")
-        genepanel_symbols_ids = self.insert(insert_stmt=self.stmt_genepanel_symbol_insert, values=genepanel_symbols,
+        genepanel_symbols_ids = self.insert(insert_stmt=self.stmt_genepanel_symbol_insert, values=all_genepanel_symbols,
                                             return_ids=True, select_stmt=self.stmt_genepanel_symbol_select,
                                             col_as_key="symbol")
+        genepanel_ids = self.insert(insert_stmt=self.stmt_genepanel_insert, values=all_genepanels, return_ids=True,
+                                    select_stmt=self.stmt_genepanel_select, col_as_key="abbreviation")
+        inheritance_ids = self.insert(insert_stmt=self.stmt_inheritance_type_insert, values=all_inheritace_types,
+                                      return_ids=True, select_stmt=self.stmt_inheritance_type_select,
+                                      col_as_key="type")
+        self.session.commit()
         # DatabaseInserter.insert_genepanel_symbol(session, genepanel_symbols)
         # DatabaseInserter.link_gene_alias(session, rows)
         print(f"Verwerktijd: {time.perf_counter() - starttijd}")
@@ -67,7 +85,6 @@ class DatabaseInserter:
         if return_ids is True and select_stmt is not None and col_as_key is not None:
             values = {"values": [value[col_as_key] for value in values]}
             results = self.session.execute(statement=select_stmt, params=values)
-            # test = {result[0]: result[1] for result in results}
             return {result[0]: result[1] for result in results}
 
     @staticmethod
@@ -103,19 +120,7 @@ def update_genepanel_v2():
     path = os.path.join(os.path.dirname(__file__), "GenPanelOverzicht_DG-3.1.0_HAN_original_tsv.txt")
     if os.path.exists(path):
         file = reader.get_reader(path)
-        session = db.session
-        all_rows: List[Mapping[str, Mapping[str, str]]] = list()
-        for line in file:
-            gene = line.gene
-            row = {"gene": {"ncbi_gene_id": gene.ncbi_gene_id, "hgnc_symbol": gene.hgnc_symbol, "in_genepanel": True},
-                   "aliases": []}
-            for alias in line.alias:
-                if alias.hgnc_symbol is not None and alias.hgnc_symbol != "":
-                    row["aliases"].append({"hgnc_symbol": alias.hgnc_symbol})
-            row["genepanel_symbol"] = {"symbol": line.p_symbol.symbol}
-            all_rows.append(row)
-
         print("Executing . . .")
         inserter = DatabaseInserter()
-        inserter.insert_genepanel(all_rows)
+        inserter.insert_genepanel(file)
         print("Done")
