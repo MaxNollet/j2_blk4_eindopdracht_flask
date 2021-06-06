6255a85d71cb3041845fa0422830d1f6aee0eff3
MaxNollet <mjh.nollet@student.han.nl>
2021-06-06 22:01:52 +0200

    Begin gemaakt aan het ophalen van resultaten uit de db

--- gaps/blueprint_results.py
+++ gaps/blueprint_results.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, url_for
+from sqlalchemy import select
+
+from gaps.models import *
 
 blueprint_results = Blueprint("blueprint_results", __name__)
 
@@ -10,6 +13,26 @@ def results():
 
     :return Rendered template of 'homepage.html' (str).
     """
+    joins = select([Gene.hgnc_symbol, Article.doi, Journal.name])\
+        .join(t_article_gene, Gene.id == t_article_gene.c.gene_id)\
+        .join(Article, Article.id == t_article_gene.c.article_id)\
+        .join(Journal)\
+        .join(t_query_gene, Gene.id == t_query_gene.c.gene_id)\
+        .join(Query, Query.id == t_query_gene.c.query_id)\
+        .where(Query.id == "df56db6d-ce3a-4aa3-b91e-48c455355f95")\
+        .order_by(Gene.hgnc_symbol)
+    print(joins)
+    query_results = db.session.execute(joins)
+    retrieved_values = list()
+    for rowproxy in query_results:
+        row = dict()
+        for field, value in zip(rowproxy._fields, rowproxy._data):
+            row[field] = value
+        retrieved_values.append(row)
+    for jup in retrieved_values:
+        print(jup)
+    # for row in results:
+    #     print(row.__dict__)
     return render_template("template_results.html")
     # search = request.args.get("search")
     # return render_template("template_query_builder.html", active="home", search=search)
