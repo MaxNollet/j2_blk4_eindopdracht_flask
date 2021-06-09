8554d229faa3dcc35fe1c8ff14cf37f23aa42e2f
MaxNollet <mjh.nollet@student.han.nl>
2021-06-09 10:16:26 +0200

    Group-by gestart

--- gaps/blueprint_results.py
+++ gaps/blueprint_results.py
@@ -21,10 +21,30 @@ def results(query_id: str):
     # In genepanel, Inheritance type, Inheritance type ID, Journal ID,
     # Journal name, NCBI gene ID, Query, Query ID.
     # See builder.get_field_names().
-    fields = ("Gene symbol", "NCBI gene ID", "In genepanel", "Abstract title", "Article PubMed ID",
+    fields = ("Gene symbol", "NCBI gene ID", "In genepanel", "Article title", "Article PubMed ID",
               "Article publication date", "Disease", "Disease MESH ID")
     builder = SelectStatementBuilder(fields, query_id)
     query_results = db.session.execute(builder.get_statement())
-    for result in query_results:
-        print(result)
-    return render_template("template_results.html")
+
+    test_results = list()
+    group_by = "Gene symbol"
+    group_index = fields.index(group_by)
+    cache_element = None
+    cache_group = None
+    # for result in query_results:
+    #     element = result[group_index]
+    #     if element != cache_element:
+    #         if cache_group is not None:
+    #             print(cache_group)
+    #             test_results.append(cache_group)
+    #         cache_group = list()
+    #     cache_element = result[group_index]
+    #     for index, column in enumerate(result):
+    #         if len(cache_group) <= index:
+    #             cache_group.append(list())
+    #         group = cache_group[index]
+    #         if column not in group:
+    #             group.append(column)
+        # print(result)
+
+    return render_template("template_results.html", fields=fields, results=query_results)
